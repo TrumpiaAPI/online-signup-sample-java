5298b6d9366eebf20fc54f0a856fed6929129b7a
Trumpia <apisupport@mytrum.com>
2018-01-24 14:29:38 -0800

    First upload

---
+++ src/main/java/com/trumpia/signup/OnlineSignupSampleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.trumpia.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSignupSampleJavaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OnlineSignupSampleJavaApplication.class, args);
+    }
+}
